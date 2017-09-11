373320bd91d37850f8bfc4e163be89ff7d6276c3
jojo <aviary256@mail.com>
2017-09-11 17:19:05 +0000

    first commit

---
+++ example.c
@@ -0,0 +1,11 @@
+#include "header1.h"
+
+int main()
+{
+  int result;
+
+  result = addition(1,2);
+  printf("addition result is : %d\n",result);
+  result = multiplication(3,2);
+  printf("multiplication result is :  %d\n",result);
+}
---
+++ header.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int addition(int a,int b);
+int multiplication(int a,int b);
+
